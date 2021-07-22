d9b1ea09fb86b2983b5f03088952a8e82a7b7c84
computeiros <drivecomputeiros@gmail.com>
2021-07-22 00:58:57 -0300

    sliderResponsiveDone

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,45 @@
+import { useState } from 'react'
 import styles from './styles.module.scss';
 
 export function Header() {
+  const [navBarMobile, setNavBarMobile] = useState(false);
+
+  const showNavBar = () => setNavBarMobile(!navBarMobile);
+
   return (
-    <header className={styles.headerContainer}>
-      <nav className={styles.navbar}>
-        <div className={styles.menuLeft}>
-          <img src="/logo.svg" alt="logo adriana langerie" />
-          <a href="" className="link">Conjuntos</a>
-          <a href="" className="link">T Shirts</a>
-        </div>
+    <>
+      <header className={styles.headerContainer}>
+        <nav className={styles.navbar}>
+          <div className={styles.menuLeft}>
+            <img src="/logo.svg" alt="logo adriana langerie" />
+            <a href="#" className={styles.link}>Conjuntos</a>
+            <a href="#" className={styles.link}>T Shirts</a>
+          </div>
 
-        <div className={styles.menuRight}>
-          <a href="#" className="link">
-            <img src="whatsapp.svg" alt="whatsapp icon" />
-            <p>Contate por whatsapp</p>
-          </a>
-        </div>
+          <div className={styles.menuRight}>
+            <a href="#">
+              <img src="whatsapp.svg" alt="whatsapp icon" />
+              <p>Contate por whatsapp</p>
+            </a>
+          </div>
 
-        <div className={styles.menuMobile}>
-          
-        </div>
-      </nav>
-    </header>
+          <div className={styles.toggleMenu}>
+            <input id={styles.menuMobile} type="checkbox" hidden onClick={showNavBar}></input>
+            <label htmlFor={styles.menuMobile}>
+              <div className={styles.menuMobile}/>
+            </label>
+          </div>
+        </nav>
+      </header>
+      {navBarMobile && (
+        <nav className={styles.openNav}>
+          <div className={styles.openNavContent}>
+            <a href="" className="link">Conjuntos</a>
+            <a href="" className="link">T Shirts</a>
+            <a href="#" className="link">Contate por whatsapp</a>
+          </div>
+        </nav>
+      )}
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,75 @@
+import {GetStaticProps} from 'next';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {
+  Pagination,Navigation,Autoplay
+} from 'swiper/core';
+
 import { Header } from '../components/Header';
 import { Background } from '../components/Background';
+
+import styles from './home.module.scss';
+
+SwiperCore.use([Autoplay,Pagination,Navigation]);
+
+//Tipagem da requisicao da API
+// type HomeProps = {
+
+// }
 
 export default function Home() {
   return (
     <>
       <Header />
       <Background />
+
+      <section className={styles.sliderContainer}>
+        <div className={styles.sliderContent}>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            loop={true}
+            pagination={{
+              "clickable": true
+            }} 
+            navigation={true}
+            // autoplay={{
+            //   "delay": 3000,
+            //   "disableOnInteraction": false
+            // }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>
+              <div className={styles.slider}>
+                <header className={styles.sliderHeader}>
+                  <img src="/TextSlide1.svg" alt="" />
+                </header>
+                <div className={styles.sliderImg}>
+                  <div className={styles.sliderItem}>
+                    <img src="/mulher1.svg" alt="" />
+                  </div>
+
+                  <div className={styles.sliderItem}>
+                    <img src="/mulher2.svg" alt="" />
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+          </Swiper>
+        </div>
+      </section>
     </>
   )
+}
+
+export const getStaticProps:GetStaticProps = async () => {
+  return {
+    props: {
+
+    },
+    revalidate: 60 * 60 * 15,
+  }
 }
